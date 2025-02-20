0207e8d5078126096be51313bc9397f702be7d3f
JustinRademaker1994 <j.a.rademaker@student.tudelft.nl>
2025-02-20 22:05:10 +0100

    step 15: add another brick

--- main.py
+++ main.py
@@ -28,8 +28,10 @@ paddle_y = SCREEN_HEIGHT - PADDLE_HEIGHT
 paddle_speed_right = 10
 paddle_speed_left = -1 * paddle_speed_right
 game_status_msg = "Controls: A and D"
-brick_x = 20
-brick_y = 20
+brick_x1 = 30
+brick_y1 = 150
+brick_x2 = brick_x1 + BRICK_WIDTH
+brick_y2 = brick_y1
 #
 # init game
 #
@@ -57,18 +59,19 @@ spritesheet = pygame.image.load('Breakout_Tile_Free.png').convert_alpha()
 ball_img = pygame.Surface((64, 64), pygame.SRCALPHA)  
 paddle_img = pygame.Surface((243, 64), pygame.SRCALPHA)
 brick_img = pygame.Surface((384, 128), pygame.SRCALPHA)
-
+brick_img2 = pygame.Surface((384, 128), pygame.SRCALPHA)
 
 # draws the ball from the specific coordinates of the spritesheet on the created surface, with a width and height of 64 pixels
 ball_img.blit(spritesheet, (0, 0), (1403, 652, 64, 64))
 paddle_img.blit(spritesheet, (0, 0), (1158, 396, 243, 64))
 brick_img.blit(spritesheet, (0, 0), (772, 390, 384, 128))
+brick_img2.blit(spritesheet, (0, 0), (386, 390, 384, 128))
 
 # Scales the surface to the desired width and height
 ball_img = pygame.transform.scale(ball_img, (BALL_WIDTH, BALL_HEIGHT))  
 paddle_img = pygame.transform.scale(paddle_img, (PADDLE_WIDTH, PADDLE_HEIGHT))
 brick_img = pygame.transform.scale(brick_img, (BRICK_WIDTH, BRICK_HEIGHT))
-
+brick_img2 = pygame.transform.scale(brick_img, (BRICK_WIDTH, BRICK_HEIGHT))
 
 #
 # game loop
@@ -130,20 +133,30 @@ while running:
        ball_speed_y = abs(ball_speed_y) * -1
 
       # bounce of brick
-    if (ball_x + BALL_WIDTH > brick_x and
-        ball_x < brick_x + BRICK_WIDTH and
-        ball_y + BRICK_HEIGHT > brick_y and
-        ball_y < brick_y + BRICK_HEIGHT):
+    if (ball_x + BALL_WIDTH > brick_x1 and
+        ball_x < brick_x1 + BRICK_WIDTH and
+        ball_y + BRICK_HEIGHT > brick_y1 and
+        ball_y < brick_y1 + BRICK_HEIGHT):
        print('brick touched at ball_x = ' + str(ball_x) + ' and ball_y = ' + str(ball_y))
 
-       if (ball_speed_y > 0 and ball_y < brick_y):
+       if (ball_speed_y > 0 and ball_y < brick_y1):
           ball_speed_y = abs(ball_speed_y) * -1
-       elif (ball_speed_y < 0 and ball_y + BALL_WIDTH > brick_y + BRICK_HEIGHT):
+       elif (ball_speed_y < 0 and ball_y + BALL_WIDTH > brick_y1 + BRICK_HEIGHT):
           ball_speed_y = abs(ball_speed_y)
-       elif (ball_speed_x > 0 and ball_x < brick_x):
+       elif (ball_speed_x > 0 and ball_x < brick_x1):
           ball_speed_x = abs(ball_speed_x) * -1
-       elif (ball_speed_x < 0 and ball_x + BALL_WIDTH > brick_x + BRICK_WIDTH):
+       elif (ball_speed_x < 0 and ball_x + BALL_WIDTH > brick_x1 + BRICK_WIDTH):
           ball_speed_x = abs(ball_speed_x)
+
+       if (ball_speed_y > 0 and ball_y < brick_y2):
+          ball_speed_y = abs(ball_speed_y) * -1
+       elif (ball_speed_y < 0 and ball_y + BALL_WIDTH > brick_y2 + BRICK_HEIGHT):
+          ball_speed_y = abs(ball_speed_y)
+       elif (ball_speed_x > 0 and ball_x < brick_x2):
+          ball_speed_x = abs(ball_speed_x) * -1
+       elif (ball_speed_x < 0 and ball_x + BALL_WIDTH > brick_x2 + BRICK_WIDTH):
+          ball_speed_x = abs(ball_speed_x)
+
     # 
     # draw everything
     #
@@ -157,7 +170,8 @@ while running:
     screen.blit(paddle_img, (paddle_x, paddle_y))
     game_status_img = font.render(game_status_msg, True, 'green')
     screen.blit(game_status_img, (SCREEN_WIDTH / 2 - game_status_img.get_width() / 2, 0))
-    screen.blit(brick_img, (brick_x, brick_y))
+    screen.blit(brick_img, (brick_x1, brick_y1))
+    screen.blit(brick_img2, (brick_x2, brick_y2))
 
     # show screen
     pygame.display.flip() 
