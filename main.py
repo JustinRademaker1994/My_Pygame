6da514fd3cf0578d57b8f11607f82f5aa5adb0df
JustinRademaker1994 <j.a.rademaker@student.tudelft.nl>
2025-02-20 22:24:22 +0100

    Step 18: use for-loop when colliding with blocks

--- main.py
+++ main.py
@@ -135,30 +135,22 @@ while running:
         ball_y < paddle_y + PADDLE_HEIGHT):
        ball_speed_y = abs(ball_speed_y) * -1
 
+    for i in range(0, len(bricks_x)):   
       # bounce of brick
-    if (ball_x + BALL_WIDTH > brick_x1 and
-        ball_x < brick_x1 + BRICK_WIDTH and
-        ball_y + BRICK_HEIGHT > brick_y1 and
-        ball_y < brick_y1 + BRICK_HEIGHT):
-       print('brick touched at ball_x = ' + str(ball_x) + ' and ball_y = ' + str(ball_y))
+      if (ball_x + BALL_WIDTH > bricks_x[i] and
+          ball_x < bricks_x[i] + BRICK_WIDTH and
+          ball_y + BRICK_HEIGHT > bricks_y[i] and
+          ball_y < bricks_y[i] + BRICK_HEIGHT):
+        print('brick touched at ball_x = ' + str(ball_x) + ' and ball_y = ' + str(ball_y))
 
-       if (ball_speed_y > 0 and ball_y < brick_y1):
-          ball_speed_y = abs(ball_speed_y) * -1
-       elif (ball_speed_y < 0 and ball_y + BALL_WIDTH > brick_y1 + BRICK_HEIGHT):
-          ball_speed_y = abs(ball_speed_y)
-       elif (ball_speed_x > 0 and ball_x < brick_x1):
-          ball_speed_x = abs(ball_speed_x) * -1
-       elif (ball_speed_x < 0 and ball_x + BALL_WIDTH > brick_x1 + BRICK_WIDTH):
-          ball_speed_x = abs(ball_speed_x)
-
-       if (ball_speed_y > 0 and ball_y < brick_y2):
-          ball_speed_y = abs(ball_speed_y) * -1
-       elif (ball_speed_y < 0 and ball_y + BALL_WIDTH > brick_y2 + BRICK_HEIGHT):
-          ball_speed_y = abs(ball_speed_y)
-       elif (ball_speed_x > 0 and ball_x < brick_x2):
-          ball_speed_x = abs(ball_speed_x) * -1
-       elif (ball_speed_x < 0 and ball_x + BALL_WIDTH > brick_x2 + BRICK_WIDTH):
-          ball_speed_x = abs(ball_speed_x)
+        if (ball_speed_y > 0 and ball_y < bricks_y[i]):
+            ball_speed_y = abs(ball_speed_y) * -1
+        elif (ball_speed_y < 0 and ball_y + BALL_WIDTH > bricks_y[i] + BRICK_HEIGHT):
+            ball_speed_y = abs(ball_speed_y)
+        elif (ball_speed_x > 0 and ball_x < bricks_x[i]):
+            ball_speed_x = abs(ball_speed_x) * -1
+        elif (ball_speed_x < 0 and ball_x + BALL_WIDTH > bricks_x[i] + BRICK_WIDTH):
+            ball_speed_x = abs(ball_speed_x)
 
     # 
     # draw everything
